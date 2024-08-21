a84219e6b42e79e51aad463f32108c846bff846f
Lorenzo Pezzotti <lorenzo.pezzotti01@gmail.com>
2024-08-21 14:24:27 +0200

    Rm StepAction class and SDAction example code
    Remove the StepAction class, it is not used in this workflow. Remove
    example code in SDAction::process method not needed.

--- DRdetector/DRsensitive/src/DREndcapTubesSDAction.cpp
+++ DRdetector/DRsensitive/src/DREndcapTubesSDAction.cpp
@@ -241,45 +241,6 @@ namespace dd4hep {
       else { // if the hit exists already, increment its fields
         hit->energyDeposit += signalhit*1000;
       }
- 
-      /*Position  prePos    = h.prePos();
-      Position  postPos   = h.postPos();
-      Position  direction = postPos - prePos;
-      Position  pos       = mean_direction(prePos,postPos);
-      Direction mom       = 0.5 * (h.preMom() + h.postMom());
-      double    hit_len   = direction.R();
-      double    depo      = h.deposit();
-      double    tim       = h.track->GetGlobalTime();
-      // Somehow extract here the physics you want
-      MyTrackerSD::Hit* hit = 
-	new MyTrackerSD::Hit(h.trkID(), h.trkPdgID(), depo, tim, hit_len, pos, mom);
-      Geant4HitData::MonteCarloContrib contrib = Geant4HitData::extractContribution(step);
-      hit->cellID        = cellID(step);
-      hit->step_length   = hit_len;
-      hit->prePos        = prePos;
-      hit->postPos       = postPos;
-      collection(m_collectionID)->add(hit);
-      mark(h.track);
-      if ( 0 == hit->cellID )  {
-        hit->cellID = volumeID(step);
-        except("+++ Invalid CELL ID for hit!");
-      }
-      printP1("Hit with deposit:%f  Pos:%f %f %f ID=%016X",
-	      depo, pos.X(), pos.Y(), pos.Z(), (void*)hit->cellID);
-      Geant4TouchableHandler handler(step);
-      print("    Geant4 path:%s", handler.path().c_str());
-
-      // Do something with my personal data (can be also something more clever ;-):
-      m_userData.integratedDeposit += contrib.deposit;
-      ++m_userData.mumDeposits;
-
-      /// Let's play with the Geant4TrackInformation
-      /// See issue https://github.com/AIDASoft/DD4hep/issues/1073
-      if ( nullptr == h.track->GetUserInformation() )   {
-	auto data = std::make_unique<ParticleUserData>();
-	data->absolute_momentum = h.track->GetMomentum().mag();
-	h.track->SetUserInformation(new Geant4ParticleInformation(std::move(data)));
-      }*/
 
       return true;
     } // end of Geant4SensitiveAction::process() method specialization
