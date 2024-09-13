d4fa852ea9aa91a3008ea2acea9515dd294d41df
Lorenzo Pezzotti <lorenzo.pezzotti01@gmail.com>
2024-09-13 15:23:14 +0200

    Apply clang-format to sensitive part
    Apply clang-format to DRsensitive *.hh and *.cpp code.

--- DRdetector/DRsensitive/include/DREndcapTubesEvtAction.hh
+++ DRdetector/DRsensitive/include/DREndcapTubesEvtAction.hh
@@ -6,44 +6,46 @@
 //**************************************************************************
 
 #ifndef DREndcapTubesEvtAction_h
-#define DREndcapTubesEvtAction_h 1
+#  define DREndcapTubesEvtAction_h 1
 
 // Includers from Geant4
-#include "G4UserEventAction.hh"
-#include "globals.hh"
+#  include "G4UserEventAction.hh"
+#  include "globals.hh"
 
-namespace dd4hep {
-  namespace sim {
-    class DREndcapTubesEvtAction final : public G4UserEventAction {
-      
-      public:
-	// Constructor and destructor
-	//
-        DREndcapTubesEvtAction();
-        virtual ~DREndcapTubesEvtAction() = default;
+namespace dd4hep
+{
+namespace sim
+{
+class DREndcapTubesEvtAction final : public G4UserEventAction
+{
+  public:
+    // Constructor and destructor
+    //
+    DREndcapTubesEvtAction();
+    virtual ~DREndcapTubesEvtAction() = default;
 
-	// Virtual methods from base class
-	//
-        virtual void BeginOfEventAction(const G4Event*) override;
-        virtual void EndOfEventAction(const G4Event*) override;
- 
-        // Inline methods
-	inline void AddEdepScin(G4double edep) { EnergyScin += edep; }
-	inline void AddSglScin(G4int scinsgl) { SglScin += scinsgl; }
-	inline void AddEdepCher(G4double edep) { EnergyCher += edep; }
-	inline void AddSglCher(G4int chersgl) { SglCher += chersgl; }
+    // Virtual methods from base class
+    //
+    virtual void BeginOfEventAction(const G4Event*) override;
+    virtual void EndOfEventAction(const G4Event*) override;
 
-      private:
-	// Fields
-	//
-	G4double EnergyScin;
-	G4int SglScin;
-	G4double EnergyCher;
-	G4int SglCher;
-    };
-  } // namespace sim
-} // namespace dd4hep
+    // Inline methods
+    inline void AddEdepScin(G4double edep) { EnergyScin += edep; }
+    inline void AddSglScin(G4int scinsgl) { SglScin += scinsgl; }
+    inline void AddEdepCher(G4double edep) { EnergyCher += edep; }
+    inline void AddSglCher(G4int chersgl) { SglCher += chersgl; }
 
-#endif // DREndcapTubesEvtAction_h
+  private:
+    // Fields
+    //
+    G4double EnergyScin;
+    G4int SglScin;
+    G4double EnergyCher;
+    G4int SglCher;
+};
+}  // namespace sim
+}  // namespace dd4hep
+
+#endif  // DREndcapTubesEvtAction_h
 
 //**************************************************************************
--- DRdetector/DRsensitive/include/DREndcapTubesRunAction.hh
+++ DRdetector/DRsensitive/include/DREndcapTubesRunAction.hh
@@ -6,45 +6,45 @@
 //**************************************************************************
 
 #ifndef DREndcapTubesRunAction_h
-#define DREndcapTubesRunAction_h 1
+#  define DREndcapTubesRunAction_h 1
 
-
-//Includers from Geant4
+// Includers from Geant4
 //
-#include "G4UserRunAction.hh"
-#include "globals.hh"
-#include "G4Timer.hh"
+#  include "G4Timer.hh"
+#  include "G4UserRunAction.hh"
+#  include "globals.hh"
 
 // Includers from project files
-//#include "DREndcapTubesEventAction.h"
+// #include "DREndcapTubesEventAction.h"
 
 // Forward declarations from Geant4
 class G4Run;
 
-namespace dd4hep {
-  namespace sim {
-    class DREndcapTubesRunAction final : public G4UserRunAction {
-            
-      public:
-        // Constructor and destructor
-        //
-        DREndcapTubesRunAction(/*DREndcapTubesEventAction* eventAction*/);
-        virtual ~DREndcapTubesRunAction() = default;
+namespace dd4hep
+{
+namespace sim
+{
+class DREndcapTubesRunAction final : public G4UserRunAction
+{
+  public:
+    // Constructor and destructor
+    //
+    DREndcapTubesRunAction(/*DREndcapTubesEventAction* eventAction*/);
+    virtual ~DREndcapTubesRunAction() = default;
 
-        // Virtual methods from base class
-        //
-        virtual void BeginOfRunAction(const G4Run*) override;
-        virtual void EndOfRunAction(const G4Run*) override;
+    // Virtual methods from base class
+    //
+    virtual void BeginOfRunAction(const G4Run*) override;
+    virtual void EndOfRunAction(const G4Run*) override;
 
-	// Fields
-        private:
-          //DREndcapTubesEventAction* fEventAction;
-	  G4Timer fTimer;
+    // Fields
+  private:
+    // DREndcapTubesEventAction* fEventAction;
+    G4Timer fTimer;
+};
+}  // namespace sim
+}  // namespace dd4hep
 
-    };
-  }
-}
-
-#endif // DREndcapTubesRunAction_h
+#endif  // DREndcapTubesRunAction_h
 
 //**************************************************************************
